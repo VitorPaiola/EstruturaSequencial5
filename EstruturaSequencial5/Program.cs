316d2009142932bd2d251fd00a3032fe832f5639
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-17 01:48:17 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaSequencial5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+string[] vet = Console.ReadLine().Split(' ');
+int cod1 = int.Parse(vet[0]);
+int num1 = int.Parse(vet[1]);
+double valor1 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+vet = Console.ReadLine().Split(' ');
+int cod2 = int.Parse(vet[0]);
+int num2 = int.Parse(vet[1]);
+double valor2 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+double total = num1 * valor1 + num2 * valor2;
+
+Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
